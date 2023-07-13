d1100aa527bc344e569520167eec19e253d24db5
Saltworker <82200669+Saltworker@users.noreply.github.com>
2023-07-13 21:44:35 +0200

    Update RegressionAnalysis_matplotlibcpp.cpp
    Ok now it is ready to go

--- RegressionAnalysis_matplotlibcpp.cpp
+++ RegressionAnalysis_matplotlibcpp.cpp
@@ -4,14 +4,14 @@
  *
  *   Regression analysis tool (class regression) 
  *   utilizing matplotlibcpp.h to graph the results.
- *   Using .txt files as input, the following is found:
+ *   Using .txt files as input, the following can be made:
  *   - Regression models
- *       ~ Linear
+ *       ~ Linear (2-part linear is an option)
  *       ~ Exponential
  *       ~ Logarithmic
  *       ~ Power
  *       ~ Second-Degree polynomium [Adjustablue in code]
- *   - Correlation coefficient(s) [R^2]
+ *   - Correlation coefficient(s) hereof [R^2]
  *   - Graph of regression models
  *   - Other statistical descriptors [MORE PENDING]
  *
